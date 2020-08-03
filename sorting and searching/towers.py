4068dd41d411922c675d40fdd70d2ff643ba8976
patrickzebinghe <patrick2hb@hotmail.com>
2020-08-03 00:09:09 -0400

    amitwani

--- sorting and searching/towers.py
+++ sorting and searching/towers.py
@@ -1,6 +1,20 @@
+import bisect
 n = int(input())
 
-blocks = sorted([int(x) for x in input().strip().split()],reverse = True)
+blocks = [int(x) for x in input().strip().split()]
 
+bases = []
+    
+bases.append(blocks[0])
+
+for i,b in enumerate(blocks):
+    if i == 0:
+        continue
+    if b >= bases[-1]:
+        bases.append(b)
+    else:
+        k = bisect.bisect_right(bases,b)
+        bases[k] = b
+print(len(bases))
 
 
